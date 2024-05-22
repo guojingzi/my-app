1b7971e5b1bd1c7cfc00d8d5f3bae6c2ddd9f883
Guo, Jingfeng <jingfeng.guo@intel.com>
2024-05-22 03:40:18 +0000

    update

--- pkg/test/README.md
+++ pkg/test/README.md
@@ -1,3 +1,5 @@
-# Top Level Product
+# Extensions API Demo
 
-Example Rancher Extension that adds a top level product with a custom page and a resource page to your Rancher Dashboard.
+Example Rancher Extension that adds several examples of the Extensions API usage to the UI.
+
+Code sample used can be found in: https://github.com/rancher/ui-plugin-examples/tree/main/pkg/extensions-api-demo/index.ts
--- pkg/test/index.ts
+++ pkg/test/index.ts
@@ -1,16 +1,74 @@
 import { importTypes } from '@rancher/auto-import';
-import { IPlugin } from '@shell/core/types';
-import extensionRouting from './routing/extension-routing';
+import {
+  IPlugin,
+  ActionLocation,
+  PanelLocation,
+  TabLocation,
+  CardLocation,
+  TableColumnLocation,
+  ActionOpts,
+} from '@shell/core/types';
 
+
+var InternalIP = "127.0.0.1"
 // Init the package
 export default function(plugin: IPlugin) {
   // Auto-import model, detail, edit from the folders
   importTypes(plugin);
-  // Provide extension metadata from package.json
-  // it will grab information such as `name` and `description`
+
+  // Provide plugin metadata from package.json
   plugin.metadata = require('./package.json');
+
   // Load a product
-  plugin.addProduct(require('./product'));
-  // Add Vue Routes
-  plugin.addRoutes(extensionRouting);
+  // plugin.addProduct(require('./product'));
+
+  // HEADER ACTION - GLOBAL
+
+  // ADD A COL TO A TABLE
+  plugin.addTableColumn(
+    TableColumnLocation.RESOURCE,
+    { resource: ['node'] },
+    {
+      name:     'some-prop-col',
+      labelKey: 'onetouch Url',
+      getValue: (row: any) => {
+        console.log(row)
+        InternalIP = row.status.addresses[0].address+":30003";
+        return {url: "http://" + row.status.addresses[0].address+":30003"};
+      },
+      formatter: 'Link',
+      formatterOpts: {
+        label: 'onetouch',
+        // urlKey: "http://www.baidu.com",
+        // target: "http://www.baidu.com",
+        // options: {
+        //   href: "http://www.baidu.com",
+        //   urlKey: 'http://www.baidu.com',
+        //   target: "http://www.baidu.com",
+        //   rel: "http://www.baidu.com"
+        // } 
+      }, 
+    }
+  );
+}
+
+export const ONETOUCHURL = {
+  name: 'OneTouchURL',
+  label: 'OneTouchURL',
+
+  value: (row: any) => {
+    
+    const internalExternalIP = row.internalIp;
+    return internalExternalIP + ":30003";
+  },
+  formatter: 'LinkToURL',
+  formatterOpts: { 
+
+    options: { 
+      internal: true,
+      target: '_blank',
+
+    } 
+  }, 
+
 }
